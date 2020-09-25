4b018bf7b570bda98bb45cbc2217d982a8bc1ba6
venkeyvenkatesh <42239849+venkeyvenkatesh@users.noreply.github.com>
2020-09-25 18:43:32 +0530

    Update Program.cs

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!1234");
+            Console.WriteLine("Hello World!");
            
         }
         static void add()
